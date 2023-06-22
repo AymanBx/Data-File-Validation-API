459d06530f26d2f115c539444462e7ccc4178c00
AymanBx <ayman_sandouk@uri.edu>
2023-06-22 14:30:36 -0400

    Extracted the specs from the specs file

--- Rules.md
+++ Rules.md
@@ -1,10 +1,10 @@
 ## TODOs
 * Plan on extracting the validation rules from the JSON file
-* 
 * Fix the naming within the JSON file (switched name to parameter)
 * Order the parameters within "column" based on the maxient doc?
 * Consider empty pipes.
 * Consider null pipes.
+* Figure out what other formats might be.
 
 ## Rules of the JSON file:
 * The data is in a list []
--- read_specs.py
+++ read_specs.py
@@ -3,9 +3,27 @@ import json
 with open('Maxient Spec.json') as json_file:
     specs = json.load(json_file)
 
-    print(specs[0]['fileFormat']['format'])
-    print()
 
-    print(len(specs[1]['columns'][0]))
+    if specs[0]['fileFormat']['format'] == "delimited records":
+        delimiter = specs[0]['fileFormat']['delimiter']
+    print("delimiter:", delimiter)
+    print()
+    if delimiter:
+       parameters_count = len(specs[1]['columns'])
+       print("Number of parameters:", parameters_count)
+       print()
+
+       parameters = []
+       for num in range (0,parameters_count):
+           parameter_name = specs[1]['columns'][num]['parameter']
+           parameter_type = specs[1]['columns'][num]['type']
+           parameter_length = specs[1]['columns'][num]['maxLength']
+           parameters.append((parameter_name, parameter_type, parameter_length))
+           print(parameters[num])
+
+       
+    
+    else:
+        print("I don't know how to with these records yet!")
 
    
